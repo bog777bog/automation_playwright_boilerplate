37cf30536de8ce16764212db290b544f2f9cc292
bog777bog <kudryabogdan.777@gmail.com>
2023-02-05 01:00:15 +0200

    added test data

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,34 +1,45 @@
 
 import { test, expect } from '@playwright/test';
 import { LoginPage } from './pages/login.page';
+import { loginData } from '../test_data/login.data';
+import { urlsData } from '../test_data/urls.data';
+
+
+test.beforeEach(async ({ page }, testInfo) => {
+    console.log(`Running ${testInfo.title}`);
+    await page.goto(urlsData.baseUrl);
+  });
 
 test('standart user is able to login', async ({ page }) => {
-  const loginPage = new LoginPage(page);
+    const loginPage = new LoginPage(page);
 
-  await page.goto('https://www.saucedemo.com');
-  await loginPage.login('standard_user', 'secret_sauce');
-  await loginPage.checkLoginButtonIsNotVisible();
+    await loginPage.login(loginData.validUserEmail, loginData.password);
+    await loginPage.checkLoginButtonIsNotVisible();
 
-  await page.waitForURL('https://www.saucedemo.com/inventory.html');
+    await page.waitForURL(urlsData.plpUrl);
 });
 
 test('problem user is able to login', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  
-  await page.goto('https://www.saucedemo.com');
-  await loginPage.login('problem_user', 'secret_sauce');
+    const loginPage = new LoginPage(page);
+    
+    await loginPage.login(loginData.problemUserEmail, loginData.password);
 
- await page.waitForURL('https://www.saucedemo.com/inventory.html');
+    await page.waitForURL(urlsData.plpUrl);
 });
 
-
 test('locked user is not able to login and error message is displayed on Login page', async ({ page }) => {
-  const loginPage = new LoginPage(page);
+    const loginPage = new LoginPage(page);
 
-  await page.goto('https://www.saucedemo.com');
-  await loginPage.login('locked_out_user', 'secret_sauce');
+    await loginPage.login(loginData.lockedUserEmail, loginData.password);
 
-  expect(await loginPage.getErrorMessage()).toMatch('Epic sadface: Sorry, this user has been locked out.');
+    expect(await loginPage.getErrorMessage()).toMatch(loginData.lockedUserErrorMessage);
+});
+
+test('it is not allowed to login with invalid creds', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.login('invalid', 'invalid');
+    expect(await loginPage.getErrorMessage()).toMatch(loginData.invalidCredsErrorMessage);
 });
 
 
